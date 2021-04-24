7d38fe123ee36e9c1275bca050f1401e3fcbdd18
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-04-24 14:30:06 -0600

    Added selection check boxes.

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var numbers = [];
+var special = []
+
+var length = "" //8-128char
+var types = ["lowercase"= true, "uppercase" = true, "numeric" = true, "special" = true]
+
+
+window.display ("Choose the following parameters for the password generator:")
+
+;if () {
+  var lowercase = false;
+}
+
+
