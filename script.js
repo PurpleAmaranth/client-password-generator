46b327b8428b4d11dde91bd47f271d29c68ea5d7
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-04-26 09:41:26 -0600

    Added symbol lists and another checkbox for punctuation marks.

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-// var numbers = [];
-// var special = []
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var numbers = ["1","2","3","4","5","6","7","8","9"];
+var special = ["!","@","#","$","%","^","&","*","?"];
+var punctuation = ["(",")","<",">",":",";",",","{","}","[","]","=","-",".","~",",","/","\\","|"];
 
 // var length = "" //8-128char
 // var types = ["lowercase"= true, "uppercase" = true, "numeric" = true, "special" = true]
